7f935f66cd2bd4f120e6bfbac14cf59aaba1f938
mland_laptop <mland@DESKTOP-PNEL2RP>
2016-12-24 17:41:12 -0500

    trying to add webpack, because you need es6

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # redux-demo
+
+
+## getting started
+`npm start`
+
+then browse to
+http://localhost:8080/
--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-console.log("hi");
-
+import redux from 'redux';
+console.log(redux);
 
 function decrement () {
+  var value = document.getElementById("theValue").innerHTML;
+  console.log(value);
+  value -- ;
+  document.getElementById("theValue").innerHTML = value
+
+}
+
+
+function increment () {
   var value = document.getElementById("theValue").innerHTML;
   console.log(value);
   value ++ ;
