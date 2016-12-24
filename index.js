510c8c061e69cf34f4c604bcbd665428b21d9485
mland_laptop <mland@DESKTOP-PNEL2RP>
2016-12-24 16:26:28 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("hi");
+
+
+function decrement () {
+  var value = document.getElementById("theValue").innerHTML;
+  console.log(value);
+  value ++ ;
+  document.getElementById("theValue").innerHTML = value
+
+}
