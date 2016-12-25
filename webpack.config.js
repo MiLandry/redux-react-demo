dab7eb6cd22e500c9fad2b07cee8affdd2d2b6cd
mland_laptop <mland@DESKTOP-PNEL2RP>
2016-12-25 10:22:25 -0500

    stuff

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    entry: ["./app.js"] ,
+    output: {
+        filename: "bundle.js"
+    },
+
+    module: {
+
+      loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+      ]
+    },
+
+    resolve: {
+      extensions: ['', '.js']
+    }
+}
