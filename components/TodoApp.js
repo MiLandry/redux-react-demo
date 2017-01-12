197d34c84d6c30d77724e24ecea2eef661556fef
mland_laptop <mland@DESKTOP-PNEL2RP>
2017-01-11 21:00:53 -0500

    Added input field

--- components/TodoApp.js
+++ components/TodoApp.js
@@ -1,25 +1,36 @@
 import React from "react";
 
+let todoCounter = 1;
 export default class TodoApp extends React.Component {
   constructor() {
     super();
     this.onClickCB = this.onClickCB.bind(this);
+
   }
+
 
   onClickCB() {
       this.props.store.dispatch({
         type: 'ADD_TODO',
-        id: 1,
-        text: "Bullshit",
-
+        id: todoCounter,
+        text: this.input.value
       })
+      todoCounter ++;
+      this.input.value = '';
 
   };
+
+  componentWillReceiveProps() {}
+
+
 
   render() {
     return (
       <div>
         <p> to do list</p>
+        <input ref={node => {
+          this.input = node;
+        }} />
         <button onClick={this.onClickCB} > Click me </button>
         <ul>
         {this.props.todos.map( todo => 
