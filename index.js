f91fd86591228815ad931471a63829432e0067ef
mland_laptop <mland@DESKTOP-PNEL2RP>
2017-01-20 23:39:37 -0500

    starting over

--- index.js
+++ index.js
@@ -2,21 +2,17 @@ import expect from 'expect';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import deepFreeze from 'deep-freeze';
-import TodoApp from './components/TodoApp'
-import store from './store';
 
 
 
 const render = () => {
   ReactDOM.render(
-    <TodoApp {...store.getState()}
-    />,
+    <div>1</div>,
     document.getElementById('root')
     );
 
 };
 
-store.subscribe(render);
 render();
 
 
