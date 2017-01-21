7bc8f8b07e8e48ad28e5a056d2c8cfbc6dd9b87e
mland_laptop <mland@DESKTOP-PNEL2RP>
2017-01-21 15:26:14 -0500

    asnteohi

--- index.js
+++ index.js
@@ -2,17 +2,61 @@ import expect from 'expect';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import deepFreeze from 'deep-freeze';
+import { createStore } from 'redux';
 
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT' :
+      return state + 1;
+    case 'DECREMENT':
+      return state -1;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(counter);
 
 
 const render = () => {
+
   ReactDOM.render(
-    <div>1</div>,
+    <div>{store.getState}</div>,
     document.getElementById('root')
     );
 
 };
 
+
+store.subscribe(render);
+
 render();
+
+// --------test
+
+
+const testCounter = () => {
+  const before = 0;
+
+  const action = {
+    type: 'INCREMENT',
+  }
+
+  const after = 1;
+
+  deepFreeze(before);
+  deepFreeze(action);
+
+  expect (
+    counter(before, action)
+    )
+    .toEqual(after);
+};
+
+testCounter();
+
+
+
 
 
