9f0026aae719ce8d0d436601f8af2660b2e49a37
mland_laptop <mland@DESKTOP-PNEL2RP>
2017-01-21 23:10:10 -0500

    foo

--- index.js
+++ index.js
@@ -5,6 +5,47 @@ import deepFreeze from 'deep-freeze';
 import { createStore } from 'redux';
 
 
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO' :
+      return state + 1;
+    default:
+      return state;
+  }
+}
+
+const testTodos = () => {
+  const before = [];
+
+  const action = {
+    type: 'ADD_TODO',
+    id: 0,
+    text: 'learn'
+    
+  }
+
+  const after = [
+  {
+    id: 0,
+    text: 'learn',
+    completed: false
+  }];
+
+  deepFreeze(before);
+  deepFreeze(action);
+
+  expect (
+    todos(before, action)
+    )
+    .toEqual(after);
+};
+
+testTodos();
+
+
+
+//ignore everything belo
+
 const counter = (state = 0, action) => {
   switch (action.type) {
     case 'INCREMENT' :
@@ -33,7 +74,7 @@ const render = () => {
   <Counter 
     value = {store.getState()}
     onAdd= {() => {store.dispatch({type: 'INCREMENT'})}}
-    onSubtract= {() => {store.dispatch({type: 'DECREMENT'})}}
+    onSubtract= {() => {store.dispatch({type: 'DECREMENT<'})}}
   />,
   document.getElementById('root')
     );
