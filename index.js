6bbad699bbd4230f0a217263b3402727e6af3f1f
Michael-Landry <michael.landry@turner.com>
2017-04-26 13:11:22 -0400

    hooked up redux to react

--- index.js
+++ index.js
@@ -3,14 +3,22 @@ import ReactDOM from 'react-dom';
 import React , {Component} from 'react';
 import deepFreeze from 'deep-freeze';
 import { createStore, combineReducers } from 'redux';
+import counter from './reducers/counter';
 
+const store = createStore(counter);
+
+console.log(store.getState());
+
+store.dispatch({ type: 'INCREMENT'});
 
 
 const render = () => {
+  let theCounter = store.getState();
+
   ReactDOM.render(
     <div>
-    <h1>0</h1>
-    <button>+</button>
+    <h1>{theCounter}</h1>
+    <button onClick={() => store.dispatch({ type: 'INCREMENT'})}>+</button>
       
     </div>
 
@@ -22,10 +30,8 @@ const render = () => {
 };
 
 
-
-
+store.subscribe(() => {
+  render();
+});
 
 render();
-
-
-
--- tests/reducers/counter.test.js
+++ tests/reducers/counter.test.js
@@ -1,4 +1,3 @@
-
 import chai from 'chai'; 
 import counter from '../../reducers/counter';
 
