7f78dc9d0857c0e859286602902a24be55df846e
Michael-Landry <michael.landry@turner.com>
2017-04-27 16:12:35 -0400

    used combine reducers

--- reducers/todoApp.js
+++ reducers/todoApp.js
@@ -1,10 +1,18 @@
+import { combineReducers } from 'redux';
 import visibilityFilter from './visibilityFilter';
 import todos from './todos';
-const todoApp = (state = [], action) => {
-  return {
-    todos: todos(state.todos, action),
-    visibilityFilter: visibilityFilter(state.visibilityFilter, action)
-  }
-}
 
-export default todoApp;
+export default const todoApp = combineReducers(
+{
+  todos,
+  visibilityFilter
+});
+
+// const todoApp = (state = [], action) => {
+//   return {
+//     todos: todos(state.todos, action),
+//     visibilityFilter: visibilityFilter(state.visibilityFilter, action)
+//   }
+// }
+
+// export default todoApp;
