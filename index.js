a4bc490808e93aa52bd000622825c6b08e08ff40
Michael Landry <mlandryviolin@gmail.com>
2017-09-18 10:18:19 -0400

    stuff

--- index.js
+++ index.js
@@ -1,34 +1,25 @@
-import expect from 'expect';
 import ReactDOM from 'react-dom';
-import React , {Component} from 'react';
-import deepFreeze from 'deep-freeze';
+import React from 'react';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-import counter from './reducers/counter';
 import todoApp from './reducers/todoApp';
 
 import App from './components/App';
 
 const store = createStore(todoApp);
 
-console.log(store.getState());
-
-let todoId = 0;
-
-
 const render = () => {
 
+  ReactDOM.render(
+    <div>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </div>
 
-    ReactDOM.render(
-        <div>
-            <Provider store={store}>
-                <App />
-            </Provider>
-        </div>
-
-        ,
-        document.getElementById('root')
-    );
+    ,
+    document.getElementById('root')
+  );
 
 
 };
