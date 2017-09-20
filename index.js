02fd4f25ffc0ec7bcd132ec89ac4c08199085269
Michael Landry <mlandryviolin@gmail.com>
2017-09-20 09:18:26 -0400

    more refactor

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import ReactDOM from 'react-dom'
-import { createStore } from 'Redux';
+import { createStore } from 'Redux'
 
 ReactDOM.render(
-    <Provider store={createStore(todoApp)}>
-        <TodoApp />
-    </Provider>,
-    document.getElementById('root')
-);
+  <Provider store={createStore(todoApp)}>
+    <TodoApp />
+  </Provider>,
+  document.getElementById('root')
+)
 
 
 
--- src/containers/VisiblityTodoList.js
+++ src/containers/VisiblityTodoList.js
@@ -1,32 +1,23 @@
-const getVisibleTodos = (todos, filter) => {
-    switch (filter) {
-    case 'SHOW_ALL':
-        return todos;
-    case 'SHOW_COMPLETED':
-        return todos.filter(t => t.completed);
-    case 'SHOW_ACTIVE':
-        return todos.filter(t => !t.completed);
-    }
-};
+
 
 const mapStateToTodoListProps = (state) => {
-    return {
-        todos: getVisibleTodos(
-            state.todos,
-            state.visibilityFilter
-        )
-    };
-};
+  return {
+    todos: getVisibleTodos(
+      state.todos,
+      state.visibilityFilter
+    )
+  }
+}
 
 const mapDispatchToTodoListProps = (dispatch) => {
-    return {
-        onTodoClick: (id) => {
-            dispatch(toggleTodo(id));
-        }
-    };
-};
+  return {
+    onTodoClick: (id) => {
+      dispatch(toggleTodo(id))
+    }
+  }
+}
 
-const VisbilityTodoList = connect(
-    mapStateToTodoListProps,
-    mapDispatchToTodoListProps
-)(TodoList);
+const VisibilityTodoList = connect(
+  mapStateToTodoListProps,
+  mapDispatchToTodoListProps
+)(TodoList)
